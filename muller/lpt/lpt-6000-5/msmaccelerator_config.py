349e44748400f7fce4e56bff0cd3379148b85737
Matthew Harrigan <harrigan@stanford.edu>
2013-11-14 12:34:16 -0800

    Added non-trajectory info

---
+++ muller/lpt/lpt-6000-5/msmaccelerator_config.py
@@ -0,0 +1,12 @@
+c = get_config()
+c.Device.zmq_port=12345
+c.Modeler.use_custom_metric = True
+c.Modeler.custom_metric_path = 'metric.pickl'
+c.Modeler.clusterer = 'hybrid'
+c.OpenMMSimulator.report_interval = 10
+c.OpenMMSimulator.number_of_steps = 6000
+c.OpenMMSimulator.minimize = False
+c.AdaptiveServer.topology_pdb = '../../../single.pdb'
+c.BaseSampler.seed_structures = 'seed_structures.h5'
+c.CountsSampler.beta = 0
+c.Modeler.lag_time = 10
