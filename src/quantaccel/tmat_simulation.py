6ecb7a0815fe527a05ba1958bb37f79d70e5d106
Matthew Harrigan <harrigan@stanford.edu>
2014-01-15 19:04:35 -0800

    Tmat sim with different params

--- src/quantaccel/tmat_simulation.py
+++ src/quantaccel/tmat_simulation.py
@@ -1,87 +1,69 @@
-"""OpenMM simulation device. We connect to server, request a starting
-structure, and then propagate.
-"""
-#############################################################################
-# Imports
-##############################################################################
 
-import os
 
-from IPython.utils.traitlets import CInt
-import mdtraj
 import scipy.io
-
 import numpy as np
+import logging as log
 
-from ..core.device import Device
-from ..core.traitlets import FilePath
-
-
-
-    # configurables.
-    tmat_fn = FilePath('tProb.mtx', config=True, help='''
-        Path to the transition matrix from which to sample.''')
-
-    gens_fn = FilePath('Gens.h5', config=True, help='''
-        Path to the generators trajectory.''')
-
-    number_of_steps = CInt(10000, config=True, help='''
-        Number of steps of dynamics to do''')
-
-    report_interval = CInt(1000, config=True, help='''
-        Interval at which to save positions to a disk, in units of steps''')
+from msmbuilder import MSMLib as msmlib
+from scipy import linalg
+import scipy.sparse.linalg
+from matplotlib import pyplot as pp
 
 
-    t_matrix = None
-    gens = None
+class TMatSimulator(object):
 
-    def start(self):
+    @property
+    def n_states(self):
+        return self.t_matrix.shape[0]
+
+    def __init__(self, tmat_fn='ntl9.mtx'):
         # Load transition matrix
-        t_matrix = scipy.io.mmread(self.tmat_fn)
+        t_matrix = scipy.io.mmread(tmat_fn)
         t_matrix = t_matrix.tocsr()
         self.t_matrix = t_matrix
-        self.log.info('Loaded transition matrix of shape %s',
-                      self.t_matrix.shape)
+
+        self.vals, self.vecs = scipy.sparse.linalg.eigs(t_matrix)
+
+        log.info('Loaded transition matrix of shape %s',
+                 self.t_matrix.shape)
 
         # Load generators
-        self.gens = mdtraj.load(self.gens_fn)
+        #self.gens = mdtraj.load(self.gens_fn)
 
-        super(TMatSimulator, self).start()
+    def simulate(self, state_i, number_of_steps=10000, report_interval=1000,
+                 out_fn=None):
+        """We run some KMC dynamics, and then send back the results.
 
+            state_i - initial state
 
-    def simulate(self, header, content):
-        """Main method that is "executed" by the receipt of the
-        msg_type == 'simulate' message from the server.
-
-        We run some KMC dynamics, and then send back the results.
         """
-        self.log.info('Starting TMat simulation...')
+        log.info('Starting TMat simulation...')
 
-        # Get that starting state path
-        starting_state_path = content.starting_state.path
+        t_matrix = self.t_matrix
 
-	state_out = np.zeros(number_of_steps // report_interval)
+        state_out = np.zeros(number_of_steps // report_interval, dtype=int)
 
-        report = self.report_interval
+        report = report_interval
         report_toti = 0
-        for _ in xrange(self.number_of_steps):
+
+        for _ in xrange(number_of_steps):
             # Get stuff from our sparse matrix
-            t_matrix = self.t_matrix
+
             csr_slicer = slice(t_matrix.indptr[state_i], t_matrix.indptr[state_i + 1])
             probs = t_matrix.data[csr_slicer]
             colinds = t_matrix.indices[csr_slicer]
 
             # Check for normalization
             np.testing.assert_almost_equal(np.sum(probs), 1,
-                                  err_msg="TMatrix isn't row normalized.")
+                    err_msg="TMatrix isn't row normalized.")
 
             # Find our new state and translate to actual indices
             prob_i = np.sum(np.cumsum(probs) < np.random.rand())
             state_i = colinds[prob_i]
 
             # Check to see if we report
-            if report == self.report_interval:
-		state_out[report_toti] = state_i
+            if report == report_interval:
+                state_out[report_toti] = state_i
 
                 # Reset
                 report = 0
@@ -92,32 +74,159 @@ from ..core.traitlets import FilePath
         # Write
         assert report_toti == state_out.shape[0], "I did my math wrong."
 
-	#TODO filename
-        np.savetxt('simtmat.dat', state_out) 
-        self.log.info('Finished TMat simulation.')
+        if out_fn is not None:
+            np.savetxt(out_fn, state_out)
+        log.info('Finished TMat simulation.')
+
+        return state_out
+
+
+class MSM(object):
+
+    def __init__(self, lag_time=1, beta=0):
+        self.tmat = None
+        self.counts = None
+
+        self.traj_list = list()
+
+        self.lag_time = lag_time
+        self.beta = beta
+
+    def add(self, traj):
+        self.traj_list.append(traj)
+
+    def build(self, sim):
+        counts = msmlib.get_count_matrix_from_assignments(np.array(self.traj_list),
+                                                          n_states=sim.n_states,
+                                                          lag_time=self.lag_time,
+                                                          sliding_window=True)
+
+        _, tmat, _, _ = msmlib.build_msm(counts,
+                                         symmetrize='Transpose',
+                                         ergodic_trimming=False)
+        self.counts = counts
+        self.tmat = tmat
+
+    def adapt(self, n_new):
+        """Return new starting states
+
+            n_new - number of new states
+            beta - 'Temperature': \beta = 1 --> even
+                                  \beta > 1 --> refinement
+                                  \beta < 1 --> exploration
+        """
+
+        counts_per_state = np.array(self.counts.sum(axis=1)).flatten() + 10.**-8
+        weights = np.power(counts_per_state, self.beta - 1.0)
+        weights /= np.sum(weights)
+
+        cum_weights = np.cumsum(weights)
+        new_states = -1 * np.ones(n_new, dtype=int)
+
+        for i in range(n_new):
+            new_states[i] = np.sum(cum_weights < np.random.rand())
+
+        log.info("Starting from states: %s", new_states)
+
+        return new_states
+
+    def error_fro(self, sim):
+        diff = self.tmat - sim.t_matrix
+        return linalg.norm(diff.todense(), ord='fro')
+
+    def error_kl(self, sim):
+        vals, vecs = scipy.sparse.linalg.eigs(self.tmat)
+        q = vecs[:,0]
+        p = sim.vecs[:,0]
+
+        q = q / np.sum(q)
+        p = p / np.sum(p)
+
+        np.testing.assert_almost_equal(np.sum(p), 1.0)
+        np.testing.assert_almost_equal(np.sum(q), 1.0)
+
+        return np.sum(np.where(np.abs(p) > 1.e-8, p * np.log(p / q), 0))
+
+    def error(self, sim, func=error_kl):
+        return func(self, sim)
 
 
 
+class Accelerator(object):
+
+    def __init__(self, simulator, msm, n_rounds=20):
+        self.sim = simulator
+        self.msm = msm
+
+        self.n_rounds = n_rounds
+        self.errors = list()
+
+    def accelerator_loop(self, n_tpr, n_spt):
+        """Run the accelerator loop
+            n_rounds - number of rounds
+            n_tpr - # trajectories per round (paralellization)
+            n_spt - # of steps (length) per trajectory
+        """
+        n_rounds = self.n_rounds
+        starting_states = np.random.randint(low=0, high=self.sim.n_states,
+                                            size=n_tpr)
+
+        for round_i in range(n_rounds):
+            for traj_i in range(n_tpr):
+                traj = self.sim.simulate(number_of_steps=n_spt,
+                                    state_i=starting_states[traj_i],
+                                    report_interval=1)
+                self.msm.add(traj)
+
+            walltime = n_spt * (round_i + 1)
+            self.msm.build(self.sim)
+            self.errors.append([walltime, self.msm.error(self.sim)])
+            starting_states = self.msm.adapt(n_tpr)
+
+        self.errors = np.array(self.errors)
+
+    def plot_errors(self):
+        errors = self.errors
+        pp.plot(errors)
+        pp.show()
+
+class RunResult(object):
+    def __init__(self, params, errors):
+        self.params = params
+        self.errors = errors
+
+    def plot(self):
+        pp.plot(self.errors[:,0], self.errors[:,1], 'o-', label=self.params['n_spt'])
+
+def main():
+    tmat_sim = TMatSimulator('ntl9.mtx')
+
+    defaults = {'lag_time': 1, 'beta': 0, 'n_rounds': 20,
+                'n_tpr': 20, 'n_spt': 1000}
+
+    params = [
+        {'n_spt': 100, 'n_rounds': 200},
+        {'n_spt': 1000},
+    ]
+
+    multierrors = list()
+
+    for setparam in params:
+        param = dict(defaults)
+        param.update(setparam)
+        msm = MSM(lag_time=param['lag_time'], beta=param['beta'])
+        accelerator = Accelerator(tmat_sim, msm, n_rounds=param['n_rounds'])
+        accelerator.accelerator_loop(n_tpr=param['n_tpr'], n_spt=param['n_spt'])
+
+        multierrors.append(RunResult(param,accelerator.errors))
+
+    for runres in multierrors:
+        runres.plot()
+    pp.legend()
+    pp.show()
 
 
+if __name__ == "__main__":
+    log.basicConfig(level=log.DEBUG)
+    main()
 
-###############################################################################
-# Utilities
-###############################################################################
-
-def random_seed():
-    """Get a seed for a random number generator, based on the current platform,
-    pid, and wall clock time.
-
-    Returns
-    -------
-    seed : int
-        The seed is a 32-bit int
-    """
-    import platform
-    import time
-    import hashlib
-    plt = ''.join(platform.uname())
-    seed = int(hashlib.md5('%s%s%s' % (plt, os.getpid(), time.time())).hexdigest(), 16)
-
-    return seed % np.iinfo(np.int32).max
