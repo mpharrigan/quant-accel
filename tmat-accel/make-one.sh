cf98b4e1482ed629f1437af13e5e6bec3a5bba55
mpharrigan <harrigan@stanford.edu>
2014-01-30 18:19:00 -0800

    For submitting a round of tmat-accel

---
+++ tmat-accel/make-one.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo usage: $0 dirindex
+	echo NOTE: Runcopy is one less than the number specified
+	exit
+fi
+
+RUNCOPY=$[$1-1]
+DIRNAME=d-run-$1
+
+echo dirname: $DIRNAME runcopy: $RUNCOPY
+pwd
+
+mkdir $DIRNAME
+cd ./$DIRNAME
+pwd
+python ../../src/quantaccel/make_tmat_jobs.py $RUNCOPY
+python ../../src/quantaccel/submit_tmat_jobs.py
+chmod +x submitter.sh
+./submitter.sh
+
+cd ../
