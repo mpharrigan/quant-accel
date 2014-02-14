dc50a83b1bd614c0fcca6555b0e7af4dfdf5a266
mpharrigan <harrigan@stanford.edu>
2014-02-13 18:36:18 -0800

    -v sort
    (yay)

--- tmat-accel/qstat-counter.sh
+++ tmat-accel/qstat-counter.sh
@@ -1,1 +1,2 @@
-qstat | grep -E tmat-\([0-9]+\).job -o | sort -n -k6,7 | uniq -c
+qstat | grep -E tmat-\([0-9]+\).job -o | sort -V | uniq -c
+#qstat | grep -E tmat-\([0-9]+\).job -o | sort -n -k6,7 | uniq -c
