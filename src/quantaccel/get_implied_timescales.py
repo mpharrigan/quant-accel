33239c0c291819fe68fa63b4dab39b753e5aa4f9
mpharrigan <harrigan@stanford.edu>
2014-02-26 17:22:41 -0800

    Plotting

--- src/quantaccel/get_implied_timescales.py
+++ src/quantaccel/get_implied_timescales.py
@@ -14,6 +14,9 @@ kmeans.
 
 """
 
+import matplotlib
+matplotlib.use('Agg')
+
 import argparse
 import logging as log
 from msmbuilder import clustering, MSMLib as msmlib, msm_analysis as msma
@@ -22,6 +25,8 @@ import os
 import mdtraj
 import numpy as np
 import stat
+from matplotlib import pyplot as pp
+import re
 
 CLUSTER_JOB_TEMPLATE = """
 #PBS -l nodes=1:ppn=16
@@ -35,7 +40,7 @@ CLUSTER_JOB_TEMPLATE = """
 cd $PBS_O_WORKDIR
 export OMP_NUM_THREADS=8
 
-python {src_dir}/quantaccel/get_implied_timescales.py cluster --distance_cutoff 0.1 &> cluster.log
+python {src_dir}/quantaccel/get_implied_timescales.py cluster &> cluster.log
 """
 
 IT_JOB_TEMPLATE = """
@@ -120,35 +125,73 @@ def implied_timescales_func(args):
 
 def make_jobs_func(args):
     """Make a bunch of jobs for qsub
-    
+
     Properties of args:
         :begin:
         :end:
         :increment:
+        :make_cluster:
     """
     lagtimes = range(args.begin, args.end, args.increment)
     assert np.all(np.asarray(lagtimes) > 0), 'Positive lagtimes only please'
-    
+
     # Write clustering job
-    with open('cluster.job', 'w') as f:
-        f.write(CLUSTER_JOB_TEMPLATE.format(**vars(args)))
-    
+    if args.make_cluster:
+        with open('cluster.job', 'w') as f:
+            f.write(CLUSTER_JOB_TEMPLATE.format(**vars(args)))
+
     # Write lagtimes job
     qsubs = []
     for lt in lagtimes:
         with open('lagtime-%d.job' % lt, 'w') as f:
             f.write(IT_JOB_TEMPLATE.format(lagtime=lt, **vars(args)))
         qsubs.append(QSUB_TEMPLATE.format(lagtime=lt))
-    
+
     # Write submit script
     with open('submit.sh', 'w') as f:
         f.write(SUBMIT_TEMPLATE.format(qsubs=''.join(qsubs)))
-        
+
     # Make it executable
     os.chmod('submit.sh', os.stat('submit.sh').st_mode | stat.S_IEXEC)
-        
+
+
+def plot_func(args):
+    """Gather and plot implied timescales.
+
+    Properties of args:
+        :xmin:
+        :xmax:
+    """
+    lagtimes = np.zeros((0, 2))
+
+    # Join
+
+    fns = os.listdir('.')
+    for fn in fns:
+        if re.match('^lagtime-[0-9]+.npy$', fn):
+            lagtime = np.load(fn)
+            lagtimes = np.append(lagtimes, lagtime, axis=0)
+
+    np.save('lagtime-all.npy', lagtimes)
+
+    # Plot
+    pp.scatter(lagtimes[:, 0], lagtimes[:, 1])
+    pp.xlabel('lagtime')
+    pp.ylabel('implied timescale')
+
+    xmin, xmax = pp.xlim()
+    if args.xmin is not None:
+        xmin = args.xmin
+    if args.xmax is not None:
+        xmax = args.xmax
+    pp.xlim((xmin, xmax))
+    pp.yscale('log')
+
+    fig_fn = 'it-plot-%d-%d.png' % (int(xmin), int(xmax))
+    pp.savefig(fig_fn)
 
 def parse():
+    """Parse command line arguments."""
     parser = argparse.ArgumentParser(description='Get implied timescales',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     sp = parser.add_subparsers()
@@ -159,7 +202,7 @@ def parse():
     cluster_p = sp.add_parser('cluster', help='Do clustering',
                               formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     cluster_p.add_argument('--load_stride', help='stride trajs by this', default=1, type=int)
-    cluster_p.add_argument('--traj_dir', help='directory of trajectories', default='../trajs/')
+    cluster_p.add_argument('--traj_dir', help='directory of trajectories', default='../../trajs/')
     cluster_p.add_argument('--distance_cutoff', help='Distance cutoff for number of clusters',
                            default=0.2, type=float)
     cluster_p.add_argument('--assign_fn', help='Save the assignments here',
@@ -186,7 +229,20 @@ def parse():
     make_jobs_p.add_argument('end', help='last lagtime', type=int)
     make_jobs_p.add_argument('-i', '--increment', help='increment', type=int, default=1)
     make_jobs_p.add_argument('-s', '--src_dir', help='source directory', required=True)
-    make_jobs_p.set_defaults(func=make_jobs_func)
+    make_jobs_p.add_argument('-c', '--make_cluster', action='store_true',
+                             help='Make a cluster job as well if this argument is present')
+    make_jobs_p.set_defaults(func=make_jobs_func, make_cluster=False)
+
+    # -------------------------------------------------------------------------
+    # Gather and plot
+    # -------------------------------------------------------------------------
+    plot_p = sp.add_parser('plot', help='Plot implied timescales',
+                           formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    plot_p.add_argument('-xi', '--xmin', help='Minimum. If not specified, lowest', type=float)
+    plot_p.add_argument('-xa', '--xmax', help='Maximum for plot.', type=float)
+    plot_p.set_defaults(func=plot_func)
+
+
 
     args = parser.parse_args()
     log.debug(str(args))
