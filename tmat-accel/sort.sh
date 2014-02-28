899dd9af72b0c0759543c8036b3bbe7bd6000a9c
Matthew Harrigan <harrigan@stanford.edu>
2014-02-27 20:17:28 -0800

    Just copy everything

--- tmat-accel/sort.sh
+++
@@ -1,5 +0,0 @@
-for file in *.pickl; do
-    new_path=$(perl -pe 's/(result-j-runcopy-)([0-9]+)(-[0-9]+\.pickl)/j-run-$2\/$1$2$3/' <<< "$file")
-    mkdir "${new_path%/*}"
-    mv -vn "$file" "$new_path"
-done
