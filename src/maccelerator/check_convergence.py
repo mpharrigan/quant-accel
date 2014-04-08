410ccb96e9e12d658803b23bb0a1601fab94e2b7
Matthew Harrigan <harrigan@stanford.edu>
2014-04-08 15:56:54 -0700

    Format

--- src/maccelerator/check_convergence.py
+++ src/maccelerator/check_convergence.py
@@ -6,7 +6,6 @@ Created on Mar 24, 2014
 From an already built msm, check convergence and write to a file
 '''
 
-
 import argparse
 from quantaccel import centroid_movie as cm
 import os
@@ -15,9 +14,12 @@ import os
 def main(args):
     """Take argparse args and call the function."""
     check_convergence(args.round_i,
-                      centroids_fn='centroids-{how}-mk{version}-{round_i}.npy'.format(**vars(args)),
-                      tmat_fn='tmatfromclus-{how}-mk{version}-{round_i}.mtx'.format(**vars(args)),
+                      centroids_fn='centroids-{how}-mk{version}-{round_i}.npy'.format(
+                          **vars(args)),
+                      tmat_fn='tmatfromclus-{how}-mk{version}-{round_i}.mtx'.format(
+                          **vars(args)),
                       dir_path='msms')
+
 
 def check_convergence(round_i, centroids_fn, tmat_fn, dir_path):
     """For a given tmat and centroids, check convergence.
@@ -26,19 +28,23 @@ def check_convergence(round_i, centroids_fn, tmat_fn, dir_path):
     an option to use a different criterion.
     """
     result_obj = cm.CentroidResult()
-    result_obj.centroids_fn = os.path.abspath(os.path.join(dir_path, centroids_fn))
+    result_obj.centroids_fn = os.path.abspath(
+        os.path.join(dir_path, centroids_fn))
     result_obj.tmat_fn = os.path.abspath(os.path.join(dir_path, tmat_fn))
     result_obj.round_i = round_i
     result_obj.abspath = dir_path
-    _, _, _, _, _, errorval = cm.load(result_obj, helper=cm.load_project_eqdistr)
+    _, _, _, _, _, errorval = cm.load(result_obj,
+                                      helper=cm.load_project_eqdistr)
     if errorval < 0.6:
         with open('converged', 'w') as f:
             f.write("Converged at round %d\n" % round_i)
 
+
 def parse():
     """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description="Check the convergence of an msm.",
-                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser = argparse.ArgumentParser(
+        description="Check the convergence of an msm.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('round_i', type=int,
                         help="""The round to check convergence.""")
@@ -49,6 +55,7 @@ def parse():
 
     args = parser.parse_args()
     main(args)
+
 
 if __name__ == "__main__":
     parse()
