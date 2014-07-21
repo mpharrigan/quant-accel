dca87321ce84ba85cbd189558a059da6dd5d7da4
Matthew Harrigan <harrigan@stanford.edu>
2014-07-21 15:02:27 -0700

    Simplify run_alanine using utils

--- src/maccelerator/testing/run_alanine.py
+++ src/maccelerator/testing/run_alanine.py
@@ -1,6 +1,7 @@
 __author__ = 'harrigan'
 
 import maccelerator as maccel
+from maccelerator.testing.utils import get_fn, get_folder
 import os
 import logging
 
@@ -8,23 +9,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 # Change directory
 os.chdir('/home/harrigan/implement/quant-accel/july-tests/')
-alanine_refmsm_fn = '/home/harrigan/projects/quant-accel/reference/ala.msm.pickl'
-alanine_centers_fn = '/home/harrigan/projects/quant-accel/reference/ala.centers.h5'
 
-# Make a directory
-i = 0
-while True:
-    try:
-        folder_name = 'a{}'.format(i)
-        os.mkdir(folder_name)
-        break
-    except:
-        i += 1
-
-configuration = maccel.AlanineConfiguration(alanine_refmsm_fn,
-                                            alanine_centers_fn)
+# Do it
+configuration = maccel.AlanineConfiguration(get_fn('ala.msm.pickl'),
+                                            get_fn('ala.centers.h5'))
 param = maccel.AlanineParams(spt=100, tpr=1)
-run = maccel.MAccelRun(configuration, param, folder_name)
+run = maccel.MAccelRun(configuration, param, get_folder('ala'))
 run.run()
 
 
