b2d37e183d0f1aaee0a99aef203276cbe1b67165
Matthew Harrigan <harrigan@stanford.edu>
2014-07-21 15:10:48 -0700

    Remove a call to pdb in case of exception

--- src/maccelerator/convergence/centroid.py
+++ src/maccelerator/convergence/centroid.py
@@ -126,10 +126,7 @@ class EigenvecCentroid(CentroidConvergenceChecker):
         ref_val = self.ref_msm.eigenvalues_[1]
 
         # <y, Poy> / <y, y>
-        try:
-            self.applied_eigenv = refs.dot(tmat)
-        except ValueError:
-            import pdb; pdb.set_trace()
+        self.applied_eigenv = refs.dot(tmat)
         est_val = refs.dot(self.applied_eigenv.T)[0, 0] / np.dot(ref, ref)
 
         errorval = ref_val - est_val
