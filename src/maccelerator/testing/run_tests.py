15372f4940e5130808af3e8c03680a87d08491f1
Matthew Harrigan <harrigan@stanford.edu>
2014-07-21 15:10:08 -0700

    Better path finding for run_tests

--- src/maccelerator/testing/run_tests.py
+++ src/maccelerator/testing/run_tests.py
@@ -1,8 +1,11 @@
 __author__ = 'harrigan'
 
 import unittest
+from maccelerator.testing.utils import get_fn
+
 
 if __name__ == "__main__":
     runner = unittest.TextTestRunner()
-    suite = unittest.defaultTestLoader.discover('.')
+    suite = unittest.defaultTestLoader.discover(
+        get_fn('../src/maccelerator/testing/'))
     runner.run(suite)
