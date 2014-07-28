24d3d647f9d1607359cf01da227c666f13ffdb2a
Matthew Harrigan <harrigan@stanford.edu>
2014-07-28 13:05:46 -0700

    Make run_... scripts importable

--- src/maccelerator/testing/run_alanine.py
+++ src/maccelerator/testing/run_alanine.py
@@ -6,13 +6,16 @@ import maccelerator as maccel
 from maccelerator.testing.utils import get_fn, get_folder
 
 
-logging.basicConfig(level=logging.DEBUG)
+def run():
+    # Do it
+    configuration = maccel.AlanineConfiguration(get_fn('ala.msm.pickl'),
+                                                get_fn('ala.centers.h5'))
+    param = maccel.AlanineParams(spt=40, tpr=1)
+    run = maccel.MAccelRun(configuration, param, get_folder('ala'),
+                           parallel=False)
+    run.run()
 
-# Do it
-configuration = maccel.AlanineConfiguration(get_fn('ala.msm.pickl'),
-                                            get_fn('ala.centers.h5'))
-param = maccel.AlanineParams(spt=20, tpr=1)
-run = maccel.MAccelRun(configuration, param, get_folder('ala'), parallel=False)
-run.run()
 
-
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    run()
--- src/maccelerator/testing/run_muller.py
+++ src/maccelerator/testing/run_muller.py
@@ -1,30 +1,19 @@
 __author__ = 'harrigan'
 
-
-import maccelerator as maccel
-import os
 import logging
 
-logging.basicConfig(level=logging.DEBUG)
-
-# Change directory
-os.chdir('/home/harrigan/implement/quant-accel/july-tests/')
-sys_xml_fn = '/home/harrigan/projects/quant-accel/reference/muller_sys.xml'
-int_xml_fn = '/home/harrigan/projects/quant-accel/reference/muller_int.xml'
-
-# Make a directory
-i = 0
-while True:
-    try:
-        folder_name = 'r{}'.format(i)
-        os.mkdir(folder_name)
-        break
-    except:
-        i+=1
+import maccelerator as maccel
+from maccelerator.testing.utils import get_fn, get_folder
 
 
-configuration = maccel.MullerConfiguration(sys_xml_fn, int_xml_fn)
-param = maccel.MullerParams(spt=800, tpr=2)
-run = maccel.MAccelRun(configuration, param, folder_name)
-run.run()
+def run():
+    configuration = maccel.MullerConfiguration(get_fn('muller_sys.xml'),
+                                               get_fn('muller_int.xml'))
+    param = maccel.MullerParams(spt=800, tpr=2)
+    run = maccel.MAccelRun(configuration, param, get_folder('mul'))
+    run.run()
 
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    run()
