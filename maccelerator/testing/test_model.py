b7e6b4c05e2ae57d473e5bb96ebabfd5b5b527df
Matthew Harrigan <harrigan@stanford.edu>
2014-09-05 18:17:28 -0700

    Add failing tests
    Want: adapt_counts, full_counts

--- maccelerator/testing/test_model.py
+++ maccelerator/testing/test_model.py
@@ -12,7 +12,46 @@ logging.captureWarnings(True)
 logging.disable(logging.WARNING)
 
 
-class TestTmatModel1(TestCase):
+class TestTmatModel_Transitions(TestCase):
+    def make_load_trajs_func(self):
+        def load_trajs(self, traj_fns, up_to=None):
+            return [np.array([0, 1, 1, 0, 0]),
+                    np.array([1, 0, 0, 1, 1])]
+
+        return load_trajs
+
+    def setUp(self):
+        config = maccel.AlanineConfiguration()
+
+        maccel.TMatModeller.load_trajs = self.make_load_trajs_func()
+        config.apply_configuration()
+        self.param = maccel.AlanineParams(0, 0)
+        self.model = config.model(None, self.param)
+        self.config = config
+
+    def test_convergence(self):
+        converge = self.config.check_convergence(self.model, self.param)
+        self.assertFalse(converge.converged)
+
+    def test_counts(self):
+        should_be = np.zeros((20, 20))
+        # TODO
+        np.testing.assert_array_equal(self.model.full_counts, should_be_full)
+        np.testing.assert_array_equal(self.model.adapt_counts, should_be_adapt)
+        np.testing.assert_array_equal(self.model.found_states, np.array([0, 1]))
+
+    def test_tmat(self):
+        should_be = np.zeros((20, 20))
+        # TODO
+        np.testing.assert_array_equal(self.model.full_tmat, should_be)
+
+    def test_populations(self):
+        should_be = np.zeros(20)
+        # TODO
+        np.testing.assert_array_equal(self.model.full_populations, should_be)
+
+
+class TestTmatModel_LongStay(TestCase):
     def make_load_trajs_func(self):
         def load_trajs(self, traj_fns, up_to=None):
             return [np.array([3, 3, 3, 3, 3, 3, 3]),
@@ -49,10 +88,11 @@ class TestTmatModel1(TestCase):
         np.testing.assert_array_equal(self.model.full_populations, should_be)
 
 
-class TestTmatModel2(TestTmatModel1):
+class TestTmatModel_ShortStay(TestTmatModel_LongStay):
     def make_load_trajs_func(self):
         def load_trajs(self, traj_fns, up_to=None):
             return [np.array([3, 3])]
+
         return load_trajs
 
 
