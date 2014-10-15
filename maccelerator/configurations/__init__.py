500ca6ac5a0f787bad3b7baaf907700c4cd1895e
Matthew Harrigan <harrigan@stanford.edu>
2014-10-15 15:16:17 -0700

    Add slurm cluster

--- maccelerator/configurations/__init__.py
+++ maccelerator/configurations/__init__.py
@@ -2,9 +2,9 @@ from .alanine import AlanineConfiguration, AlanineParams
 from .muller import MullerConfiguration, MullerParams
 from .simple import SimpleConfiguration, SimpleParams
 from .base import TMatConfiguration
-from .cluster import PBSCluster
+from .cluster import PBSCluster, SlurmCluster
 
 
 __all__ = ['AlanineConfiguration', 'MullerConfiguration', 'SimpleConfiguration',
            'AlanineParams', 'MullerParams', 'SimpleParams', 'PBSCluster',
-           'TMatConfiguration']
+           'TMatConfiguration', 'SlurmCluster']
