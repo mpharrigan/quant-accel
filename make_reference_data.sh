2b9930d79a5369295d00b10a3b03f66b9da73dab
Matthew Harrigan <harrigan@stanford.edu>
2014-11-11 15:55:30 -0800

    Make reference data in setup.py

--- make_reference_data.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python scripts/make_reference_data.py maccelerator/reference
--- setup.py
+++ setup.py
@@ -1,4 +1,14 @@
+import os
+
 from setuptools import setup, find_packages
+
+
+REFDIR = 'maccelerator/reference'
+if not os.path.exists(REFDIR):
+    print("Making reference data")
+    import make_reference_data
+
+    make_reference_data.make_reference_data(REFDIR)
 
 setup(name='maccelerator',
       version='0.2',
@@ -6,4 +16,4 @@ setup(name='maccelerator',
       packages=find_packages(),
       scripts=['scripts/maccel.py'],
       zip_safe=False,
-      package_data={'maccelerator':['reference/*.*']})
+      package_data={'maccelerator': ['reference/*.*']})
